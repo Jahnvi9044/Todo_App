bf147c8411566ccfebb686020f4f493e7a5e2777
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2024-09-27 07:27:18 +0530

    Frontend started

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import { CreateTodo } from './components/CreateTodo'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+       <CreateTodo></CreateTodo>
+      </div>
+     
+     
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Todo.jsx
@@ -0,0 +1,9 @@
+
+export function Todo(){
+    return <div>
+        <h1>Go to Gym</h1>
+        <h2>You need to go to Gym</h2>
+        <button>Mark as completed</button>
+    </div>
+
+}
