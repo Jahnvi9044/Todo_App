14ea59b1c7492e830ca4ebc3e210f104ceeaff70
Jahnvi Chaurasia <jahnvichaurasia481@gmail.com>
2024-09-28 12:33:08 +0530

    Modified components to send request to backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,26 @@
 import { useState } from 'react'
 import { CreateTodo } from './components/CreateTodo'
-
+import { Todo } from './components/Todo';
 import './App.css'
 
+
 function App() {
- 
+  const [todos,setTodo] = useState([]);
+     
+ fetch("http://localhost:3000/todos").then(async function(res){
+  const json = await res.json();
+  setTodo(json.todos);
+
+ }) 
+  console.log(todos);
 
   return (
     <>
       <div>
        <CreateTodo></CreateTodo>
+       <Todo todos={todos}></Todo>
+      
       </div>
-     
      
     </>
   )
--- frontend/src/components/Todo.jsx
+++ frontend/src/components/Todo.jsx
@@ -1,9 +1,40 @@
 
-export function Todo(){
-    return <div>
-        <h1>Go to Gym</h1>
-        <h2>You need to go to Gym</h2>
-        <button>Mark as completed</button>
+export function Todo({todos}){
+    const [completed,setCompleted] = useState(false);
+
+    // return  // <div>
+    //     <h1>Go to Gym</h1>
+    //     <h2>You need to go to Gym</h2>
+    //     <button>Mark as completed</button>
+    // </div>
+
+    return  <div>
+        {todos.map(function(todo){
+            return  <div>
+              <h1>{todo.title}</h1>
+              <h2>{todo.description}</h2>
+              <button onClick={()=>{
+                console.log(todo._id);
+             
+                fetch("http://localhost:3000/completed",{
+                    method:"PUT",
+                    body:JSON.stringify({
+                        id:todo._id
+                    }),
+                    headers:{
+                        "Content-Type":"application/json"
+                    }
+                       
+                    
+                }).then(async function(res){
+                    const json = await res.json();
+                    alert("Todo marked true");
+                    
+                   }) 
+            }}>{todo.completed == true ? "Completed":"Mark as Completed"}</button>
+            </div>
+          
+        })}
     </div>
 
 }
